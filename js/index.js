73767e3f98fb9545afb654d49dbecf67311b9c90
MarianM <marianmelinte90@gmail.com>
2022-06-28 21:59:27 +0200

    done

--- js/index.js
+++ js/index.js
@@ -1,6 +1,52 @@
+console.log("I'm ready!");
+
 // Iteration 1: Names and Input
-//
+
+let hacker1 = 'Johnny'
+
+console.log(`The driver name is ${hacker1}`)
+
+let hacker2 = 'Emily'
+
+console.log(`The navigator name is ${hacker2}`)
+
 // Iteration 2: Conditionals
 
+if (hacker1.length > hacker2.length) {
+    console.log(`The driver has the longest name, it has ${hacker1.length} characters`)
 
-// Iteration 3: Loops
+} else if (hacker1.length < hacker2.length) {
+    console.log(`It seems that the navigator has the longest name, it has ${hacker2.length} characters.`)
+
+} else if (hacker1.length === hacker2.length) {
+    console.log(`Wow, you both have equally long names, ${hacker1.length} characters!`)
+    
+}
+
+// Iteration 3: Loopsnode
+
+const splittedString = hacker1.split('');
+const stringWithSpaces = splittedString.join(' ');
+const upperCaseStringWithSpaces = stringWithSpaces.toUpperCase();
+
+console.log(upperCaseStringWithSpaces);
+
+
+function reverseString(hacker2) {
+    const arrayStrings = hacker2.split("");
+    const reverseArray = arrayStrings.reverse();
+    const joinArray = reverseArray.join("");
+    return joinArray;
+}   
+
+const result = reverseString(hacker2);
+console.log(result);
+
+if (hacker1 > hacker2) {
+    console.log("The driver's name goes first.");
+} else if (hacker1 < hacker2) {
+    console.log("Yo, the navigator goes first definitely.");
+} else {
+    console.log("What?! You both have the same name?");
+}
+
